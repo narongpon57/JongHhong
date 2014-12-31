1f353c3748f5ccb0c30d074b4f83191fbceba2e4
narongpon57 <n.suthimakphol@gmail.com>
2014-12-30 17:09:31 -0800

    ep 4

--- app/src/main/java/com/example/narongpon/jonghhong/MainActivity.java
+++ app/src/main/java/com/example/narongpon/jonghhong/MainActivity.java
@@ -1,17 +1,48 @@
 package com.example.narongpon.jonghhong;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.gc.materialdesign.views.ButtonRectangle;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private EditText edtUser;
+    private EditText edtPass;
+    private ButtonRectangle btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.jh_login);
+
+        btnLogin = (ButtonRectangle)findViewById(R.id.btn_login);
+        edtUser = (EditText)findViewById(R.id.edt_user);
+        edtPass = (EditText)findViewById(R.id.edt_pass);
+
+        edtUser.setText("");
+        edtPass.setText("");
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txtUser = edtUser.getText().toString();
+                String txtPass = edtPass.getText().toString();
+
+                if(txtUser.equals("zdbw0057") && txtPass.equals("123456")) {
+                    Intent i = new Intent(getApplicationContext(),JHCheckRoom.class);
+                    startActivity(i);
+                }
+
+            }
+        });
+
     }
 
 
